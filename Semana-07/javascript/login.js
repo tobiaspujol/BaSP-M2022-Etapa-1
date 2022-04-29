84cb95b7cc832ebbbf26dcaab97c4403aed9f1a0
Tobías Pujol <tobipujol@gmail.com>
2022-04-29 19:01:39 -0300

    BaSP-W07-2022: Folder added and changes in Login.js and SignUp.js related to btn validation

---
+++ Semana-07/javascript/login.js
@@ -0,0 +1,82 @@
+window.onload = function() {
+    var loginEmail = document.getElementById('email');
+    var loginPassword = document.getElementById('password');
+    var emailRegExp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+    var emailInlineAlert = document.createElement('p');
+    var passwordInlineAlert = document.createElement('p');
+    emailInlineAlert.textContent = "";
+    passwordInlineAlert.textContent = "";
+    loginEmail.addEventListener('blur', validateEmailB);
+    loginEmail.addEventListener('focus', validateEmailF);
+    loginPassword.addEventListener('blur', validatePasswordB);
+    loginPassword.addEventListener('focus', validatePasswordF);
+
+    function validateEmailB() {
+        if (!emailRegExp.test(loginEmail.value)) {
+            emailInlineAlert.textContent = "* Email is not valid.";
+            loginEmail.insertAdjacentElement('afterend', emailInlineAlert);
+            return false;
+        } else {
+            emailInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validateEmailF() {
+        emailInlineAlert.remove();
+    }
+    
+    function validatePasswordB() {
+        var numbers = [0,1,2,3,4,5,6,7,8,9];
+        var num = 0;
+        var char = 0;
+        for (i=0; i < loginPassword.value.length; i++) {
+            for (j=0; j < 10; j++) {
+                if (loginPassword.value[i] == numbers[j]) {
+                    num++;
+                }
+            }
+        }
+        if (loginPassword.value.length != num) {
+            char = 1;
+        }
+        if ((num == 0) || (char == 0) || (loginPassword.value.length < 8)) {
+            passwordInlineAlert.textContent = "* Password is not valid.";
+            loginPassword.insertAdjacentElement('afterend', passwordInlineAlert);
+            return false;
+        } else {
+            passwordInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validatePasswordF() {
+        passwordInlineAlert.remove();
+    }
+
+    var continueBtn = document.getElementById('continue-btn');
+    continueBtn.addEventListener('click', btn);
+
+    function urlString (email, password) {
+        var urlArray = ["email="+email, "password="+password];
+        return urlArray.join("&");
+    }
+
+    function btn() {
+        if (validateEmailB() && validatePasswordB()) {
+            fetch('https://basp-m2022-api-rest-server.herokuapp.com/login?'+urlString(loginEmail.value,loginPassword.value))
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (responseJson) {
+                console.log(responseJson.success);
+                alert('Login successfull');
+                alert('Email: '+loginEmail.value+'  Password: '+loginPassword.value);
+            })
+            .catch(function(error) {
+                console.log(error);
+                alert('Incorrect login');
+            });
+        } else {
+            alert('Incorrect login');
+        }
+    }
+}
---
+++ Semana-07/javascript/sign-up.js
@@ -0,0 +1,298 @@
+window.onload = function() {
+
+    // containsNumber? function.
+
+    function containsNumber(val) {
+        var numbers = [0,1,2,3,4,5,6,7,8,9];
+        var num = 0;
+        for (i=0; i < val.value.length; i++) {
+            for (j=0; j < 10; j++) {
+                if ((val.value[i] == numbers[j]) && (val.value[i] != ' ')) {
+                    num++;
+                }
+            }
+        }
+        return num;
+    }
+
+    // Name and Surname valids.
+
+    var signUpName = document.getElementById('name');
+    var signUpSurname = document.getElementById('surname');
+    var nameInlineAlert = document.createElement('p');
+    var surnameInlineAlert = document.createElement('p');
+    nameInlineAlert.textContent = "";
+    surnameInlineAlert.textContent = "";
+    signUpName.addEventListener('blur', validateNameB);
+    signUpName.addEventListener('focus', validateNameF);
+    signUpSurname.addEventListener('blur', validateSurnameB);
+    signUpSurname.addEventListener('focus', validateSurnameF);
+
+    function validateNameB() {
+        var numm = 0;
+        var numm = containsNumber(signUpName);
+        if ((numm > 0) || (signUpName.value.length < 3)) {
+            nameInlineAlert.textContent = "* Name is not valid.";
+            signUpName.insertAdjacentElement('afterend', nameInlineAlert);
+            return false;
+        } else {
+            nameInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validateNameF() {
+        nameInlineAlert.remove();
+    }
+
+    function validateSurnameB() {
+        var numm = 0;
+        var numm = containsNumber(signUpSurname);
+        if ((numm > 0) || (signUpSurname.value.length < 3)) {
+            surnameInlineAlert.textContent = "* Surname is not valid.";
+            signUpSurname.insertAdjacentElement('afterend', surnameInlineAlert);
+            return false;
+        } else {
+            surnameInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validateSurnameF() {
+        surnameInlineAlert.remove();
+    }
+
+    // DNI, phone number and ZIP code valids.
+
+    var signUpDni = document.getElementById('dni');
+    var signUpPhone = document.getElementById('phone-number');
+    var signUpZip = document.getElementById('zip');
+    var dniInlineAlert = document.createElement('p');
+    var phoneInlineAlert = document.createElement('p');
+    var zipInlineAlert = document.createElement('p');
+    dniInlineAlert.textContent = "";
+    phoneInlineAlert.textContent = "";
+    zipInlineAlert.textContent = "";
+    signUpDni.addEventListener('blur', validateDniB);
+    signUpDni.addEventListener('focus', validateDniF);
+    signUpPhone.addEventListener('blur', validatePhoneB);
+    signUpPhone.addEventListener('focus', validatePhoneF);
+    signUpZip.addEventListener('blur', validateZipB);
+    signUpZip.addEventListener('focus', validateZipF);
+
+    function validateDniB() {
+        var numm = 0;
+        var numm = containsNumber(signUpDni);
+        if ((numm != signUpDni.value.length) || (signUpDni.value.length < 7)) {
+            dniInlineAlert.textContent = "* DNI is not valid.";
+            signUpDni.insertAdjacentElement('afterend', dniInlineAlert);
+            return false;
+        } else {
+            dniInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validateDniF() {
+        dniInlineAlert.remove();
+    }
+
+    function validatePhoneB() {
+        var numm = 0;
+        var numm = containsNumber(signUpPhone);
+        if ((signUpPhone.value.length != numm) || (signUpPhone.value.length != 10)) {
+            phoneInlineAlert.textContent = "* Phone is not valid.";
+            signUpPhone.insertAdjacentElement('afterend', phoneInlineAlert);
+            return false;
+        } else {
+            phoneInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validatePhoneF() {
+        phoneInlineAlert.remove();
+    }
+
+    function validateZipB() {
+        var numm = 0;
+        var numm = containsNumber(signUpZip);
+        if ((signUpZip.value.length != numm) || (signUpZip.value.length < 4) || (signUpZip.value.length > 5)) {
+            zipInlineAlert.textContent = "* ZIP code is not valid.";
+            signUpZip.insertAdjacentElement('afterend', zipInlineAlert);
+            return false;
+        } else {
+            zipInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validateZipF() {
+        zipInlineAlert.remove();
+    }
+
+    // Date of birth valid.
+
+    var signUpDob = document.getElementById('dob');
+    var dobInlineAlert = document.createElement('p');
+    dobInlineAlert.textContent = "";
+    signUpDob.addEventListener('blur', validateDobB);
+    signUpDob.addEventListener('focus', validateDobF);
+
+    function validateDobB() {
+        if ((containsNumber(signUpDob) != 8) || (signUpDob.value[2] != '/') || (signUpDob.value[5] != '/') ||
+        (Number(signUpDob.value[0]+signUpDob.value[1]) > 31) || (Number(signUpDob.value[3]+signUpDob.value[4]) > 12)
+        || (Number(signUpDob.value[6]+signUpDob.value[7]+signUpDob.value[8]+signUpDob.value[9]) > 2004)) {
+            dobInlineAlert.textContent = "* Date of birth is not valid.";
+            signUpDob.insertAdjacentElement('afterend', dobInlineAlert);
+            return false;
+        } else {
+            dobInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validateDobF() {
+        dobInlineAlert.remove();
+    }
+
+    // Address and city valids.
+
+    var signUpAddress = document.getElementById('address');
+    var signUpCity = document.getElementById('city');
+    var addressInlineAlert = document.createElement('p');
+    var cityInlineAlert = document.createElement('p');
+    addressInlineAlert.textContent = "";
+    cityInlineAlert.textContent = "";
+    signUpAddress.addEventListener('blur', validateAddressB);
+    signUpAddress.addEventListener('focus', validateAddressF);
+    signUpCity.addEventListener('blur', validateCityB);
+    signUpCity.addEventListener('focus', validateCityF);
+
+    function containsSpace(val) {
+        var space = 0;
+        for (i=0; i < 10; i++) {
+            if (val.value[i] == ' ') {
+                space++;
+            }
+        }
+        return space;
+    }
+
+    function validateAddressB() {
+        var numm = 0;
+        var numm = containsNumber(signUpAddress);
+        var char = 0;
+        var spacee = 0;
+        spacee = containsSpace(signUpAddress);
+        if (signUpAddress.value.length != numm) {
+            char = 1;
+        }
+        if ((numm == 0) || (char == 0) || (signUpAddress.value.length < 5) || (spacee == 0)) {
+            addressInlineAlert.textContent = "* Address is not valid.";
+            signUpAddress.insertAdjacentElement('afterend', addressInlineAlert);
+            return false;
+        } else {
+            addressInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validateAddressF() {
+        addressInlineAlert.remove();
+    }
+    
+    function validateCityB() {
+        var numm = 0;
+        var numm = containsNumber(signUpCity);
+        var char = signUpCity.value.length - numm;
+        if (char < 3) {
+            cityInlineAlert.textContent = "* City is not valid.";
+            signUpCity.insertAdjacentElement('afterend', cityInlineAlert);
+            return false;
+        } else {
+            cityInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validateCityF() {
+        cityInlineAlert.remove();
+    }
+
+    // Email, password and Repeat password valids.
+
+    var signUpEmail = document.getElementById('email');
+    var signUpPassword = document.getElementById('password');
+    var signUpRepPassword = document.getElementById('rep-password');
+    var emailRegExp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+    var emailInlineAlert = document.createElement('p');
+    var passwordInlineAlert = document.createElement('p');
+    var repPasswordInlineAlert = document.createElement('p');
+    emailInlineAlert.textContent = "";
+    passwordInlineAlert.textContent = "";
+    repPasswordInlineAlert.textContent = "";
+    signUpEmail.addEventListener('blur', validateEmailB);
+    signUpEmail.addEventListener('focus', validateEmailF);
+    signUpPassword.addEventListener('blur', validatePasswordB);
+    signUpPassword.addEventListener('focus', validatePasswordF);
+    signUpRepPassword.addEventListener('blur', validateRepPasswordB);
+    signUpRepPassword.addEventListener('focus', validateRepPasswordF);
+
+    function validateEmailB() {
+        if (!emailRegExp.test(signUpEmail.value)) {
+            emailInlineAlert.textContent = "* Email is not valid.";
+            signUpEmail.insertAdjacentElement('afterend', emailInlineAlert);
+            return false;
+        } else {
+            emailInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validateEmailF() {
+        emailInlineAlert.remove();
+    }
+    
+    function validatePasswordB() {
+        var char = 0;
+        var numm = 0;
+        var numm = containsNumber(signUpPassword);
+        if (signUpPassword.value.length != numm) {
+            char = 1;
+        }
+        if ((numm == 0) || (char == 0) || (signUpPassword.value.length < 8)) {
+            passwordInlineAlert.textContent = "* Password is not valid.";
+            signUpPassword.insertAdjacentElement('afterend', passwordInlineAlert);
+            return false;
+        } else {
+            passwordInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validatePasswordF() {
+        passwordInlineAlert.remove();
+    }
+
+    function validateRepPasswordB() {
+        if (signUpPassword.value != signUpRepPassword.value) {
+            repPasswordInlineAlert.textContent = "* Password must be the same.";
+            signUpPassword.insertAdjacentElement('afterend', repPasswordInlineAlert);
+            return false;
+        } else {
+            repPasswordInlineAlert.textContent = "";
+            return true;
+        }
+    }
+    function validateRepPasswordF() {
+        repPasswordInlineAlert.remove();
+    }
+
+    var continueBtn = document.getElementById('continue-btn');
+    continueBtn.addEventListener('click', btn);
+
+    function btn() {
+        if (validateNameB() && validateSurnameB() && validateDniB() && validatePhoneB() && validateZipB() && 
+        validateDobB() && validateAddressB() && validateCityB() && validateEmailB() && validatePasswordB() && 
+        validateRepPasswordB()) {
+            alert('Login successfull');
+            alert('Complete name: '+signUpName.value+' '+signUpSurname.value+'   DNI: '+signUpDni.value+
+            '   Date of birth: '+signUpDob.value+'   Phone number: '+signUpPhone.value+'   Address: '+
+            signUpAddress.value+'   City: '+signUpCity.value+'   ZIP code: '+signUpZip.value+'   Email: '+
+            signUpEmail.value+'  Password: '+signUpPassword.value);
+        } else {
+            alert('Incorrect login');
+        }
+    }
+}
